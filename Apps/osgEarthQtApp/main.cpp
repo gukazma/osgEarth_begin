3a83ad5bfec349f9bfb900d477343cc56e324c74
gukazma <gukazma@proton.me>
2023-09-06 21:37:11 +0800

    ✨ feat: 成功加载地球

--- Apps/osgEarthQtApp/main.cpp
+++ Apps/osgEarthQtApp/main.cpp
@@ -33,6 +33,22 @@
 
 #include <osgGA/Device>
 
+#include <osgEarth/MapNode>
+#include <osgEarth/SpatialReference>
+
+#include <osgEarth/EarthManipulator>
+#include <osgEarth/Ephemeris>
+#include <osgEarth/GeoCommon>
+#include <osgEarth/GeoTransForm>
+#include <osgEarth/Sky>
+#include <osgEarth/SpatialReference>
+#include <osgEarth/MapNode>
+#include <osgEarth/EarthManipulator>
+#include <osgEarth/ExampleResources>
+#include <osgEarth/MapNode>
+#include <osgEarth/Threading>
+#include <osgEarth/ShaderGenerator>
+
 #include <QApplication>
 #include <QSurfaceFormat>
 
@@ -42,6 +58,7 @@
 int main(int argc, char** argv)
 {
 
+    osgEarth::initialize();
 
     QSurfaceFormat format = QSurfaceFormat::defaultFormat();
 
@@ -68,157 +85,43 @@ int main(int argc, char** argv)
     // use an ArgumentParser object to manage the program arguments.
     osg::ArgumentParser arguments(&argc, argv);
 
-    arguments.getApplicationUsage()->setApplicationName(arguments.getApplicationName());
-    arguments.getApplicationUsage()->setDescription(
-        arguments.getApplicationName() +
-        " is the standard OpenSceneGraph example which loads and visualises 3d models.");
-    arguments.getApplicationUsage()->setCommandLineUsage(arguments.getApplicationName() +
-                                                         " [options] filename ...");
-    arguments.getApplicationUsage()->addCommandLineOption("--image <filename>",
-                                                          "Load an image and render it on a quad");
-    arguments.getApplicationUsage()->addCommandLineOption(
-        "--dem <filename>", "Load an image/DEM and render it on a HeightField");
-    arguments.getApplicationUsage()->addCommandLineOption(
-        "--login <url> <username> <password>",
-        "Provide authentication information for http file access.");
-    arguments.getApplicationUsage()->addCommandLineOption(
-        "-p <filename>",
-        "Play specified camera path animation file, previously saved with 'z' key.");
-    arguments.getApplicationUsage()->addCommandLineOption(
-        "--speed <factor>", "Speed factor for animation playing (1 == normal speed).");
-    arguments.getApplicationUsage()->addCommandLineOption("--device <device-name>",
-                                                          "add named device to the viewer");
 
     osgQOpenGLWidget widget(&arguments);
 
     QObject::connect(&widget, &osgQOpenGLWidget::initialized, [&arguments, &widget] {
-        unsigned int helpType = 0;
+        
 
-        if ((helpType = arguments.readHelpType())) {
-            arguments.getApplicationUsage()->write(std::cout, helpType);
-            return 1;
-        }
+       // This is normally called by Viewer::run but we are running our frame loop manually so we
+        // need to call it here.
+        widget.getOsgViewer()->setReleaseContextAtEndOfFrameHint(false);
 
-        // report any errors if they have occurred when parsing the program arguments.
-        if (arguments.errors()) {
-            arguments.writeErrorMessages(std::cout);
-            return 1;
-        }
+        // Tell the database pager to not modify the unref settings
+        widget.getOsgViewer()->getDatabasePager()->setUnrefImageDataAfterApplyPolicy(true, false);
 
-        if (arguments.argc() <= 1) {
-            arguments.getApplicationUsage()->write(std::cout,
-                                                   osg::ApplicationUsage::COMMAND_LINE_OPTION);
-            return 1;
-        }
+        // thread-safe initialization of the OSG wrapper manager. Calling this here
+        // prevents the "unsupported wrapper" messages from OSG
+        osgDB::Registry::instance()->getObjectWrapperManager()->findWrapper("osg::Image");
 
-        std::string url, username, password;
+        // install our default manipulator (do this before calling load)
+        widget.getOsgViewer()->setCameraManipulator(new osgEarth::EarthManipulator(arguments));
 
-        while (arguments.read("--login", url, username, password)) {
-            osgDB::Registry::instance()->getOrCreateAuthenticationMap()->addAuthenticationDetails(
-                url, new osgDB::AuthenticationDetails(username, password));
-        }
+        // disable the small-feature culling
+        widget.getOsgViewer()->getCamera()->setSmallFeatureCullingPixelSize(-1.0f);
 
-        std::string device;
+        // load an earth file, and support all or our example command-line options
+        auto node = osgEarth::Util::MapNodeHelper().load(arguments, widget.getOsgViewer());
+        if (node.valid()) {
+            widget.getOsgViewer()->setSceneData(node);
 
-        while (arguments.read("--device", device)) {
-            osg::ref_ptr<osgGA::Device> dev = osgDB::readRefFile<osgGA::Device>(device);
-
-            if (dev.valid()) {
-                widget.getOsgViewer()->addDevice(dev);
+            if (!osgEarth::Util::MapNode::get(node)) {
+                // not an earth file? Just view as a normal OSG node or image
+                widget.getOsgViewer()->setCameraManipulator(new osgGA::TrackballManipulator);
+                osgUtil::Optimizer opt;
+                opt.optimize(node, osgUtil::Optimizer::INDEX_MESH);
+                osgEarth::Util::ShaderGenerator gen;
+                node->accept(gen);
             }
         }
-
-        // set up the camera manipulators.
-        {
-            osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator =
-                new osgGA::KeySwitchMatrixManipulator;
-
-            keyswitchManipulator->addMatrixManipulator(
-                '1', "Trackball", new osgGA::TrackballManipulator());
-            keyswitchManipulator->addMatrixManipulator(
-                '2', "Flight", new osgGA::FlightManipulator());
-            keyswitchManipulator->addMatrixManipulator('3', "Drive", new osgGA::DriveManipulator());
-            keyswitchManipulator->addMatrixManipulator(
-                '4', "Terrain", new osgGA::TerrainManipulator());
-            keyswitchManipulator->addMatrixManipulator('5', "Orbit", new osgGA::OrbitManipulator());
-            keyswitchManipulator->addMatrixManipulator(
-                '6', "FirstPerson", new osgGA::FirstPersonManipulator());
-            keyswitchManipulator->addMatrixManipulator(
-                '7', "Spherical", new osgGA::SphericalManipulator());
-
-            std::string pathfile;
-            double      animationSpeed = 1.0;
-
-            while (arguments.read("--speed", animationSpeed)) {}
-            char keyForAnimationPath = '8';
-
-            while (arguments.read("-p", pathfile)) {
-                osgGA::AnimationPathManipulator* apm =
-                    new osgGA::AnimationPathManipulator(pathfile);
-
-                if (apm || !apm->valid()) {
-                    apm->setTimeScale(animationSpeed);
-
-                    unsigned int num = keyswitchManipulator->getNumMatrixManipulators();
-                    keyswitchManipulator->addMatrixManipulator(keyForAnimationPath, "Path", apm);
-                    keyswitchManipulator->selectMatrixManipulator(num);
-                    ++keyForAnimationPath;
-                }
-            }
-
-            widget.getOsgViewer()->setCameraManipulator(keyswitchManipulator.get());
-        }
-
-        // add the state manipulator
-        widget.getOsgViewer()->addEventHandler(new osgGA::StateSetManipulator(
-            widget.getOsgViewer()->getCamera()->getOrCreateStateSet()));
-
-        // add the thread model handler
-        widget.getOsgViewer()->addEventHandler(new osgViewer::ThreadingHandler);
-
-        // add the window size toggle handler
-        widget.getOsgViewer()->addEventHandler(new osgViewer::WindowSizeHandler);
-
-        // add the stats handler
-        widget.getOsgViewer()->addEventHandler(new osgViewer::StatsHandler);
-
-        // add the help handler
-        widget.getOsgViewer()->addEventHandler(
-            new osgViewer::HelpHandler(arguments.getApplicationUsage()));
-
-        // add the record camera path handler
-        widget.getOsgViewer()->addEventHandler(new osgViewer::RecordCameraPathHandler);
-
-        // add the LOD Scale handler
-        widget.getOsgViewer()->addEventHandler(new osgViewer::LODScaleHandler);
-
-        // add the screen capture handler
-        widget.getOsgViewer()->addEventHandler(new osgViewer::ScreenCaptureHandler);
-
-        // load the data
-        osg::ref_ptr<osg::Node> loadedModel = osgDB::readRefNodeFiles(arguments);
-
-        if (!loadedModel) {
-            std::cout << arguments.getApplicationName() << ": No data loaded" << std::endl;
-            return 1;
-        }
-
-        // any option left unread are converted into errors to write out later.
-        arguments.reportRemainingOptionsAsUnrecognized();
-
-        // report any errors if they have occurred when parsing the program arguments.
-        if (arguments.errors()) {
-            arguments.writeErrorMessages(std::cout);
-            return 1;
-        }
-
-
-        // optimize the scene graph, remove redundant nodes and state etc.
-        osgUtil::Optimizer optimizer;
-        optimizer.optimize(loadedModel);
-
-        widget.getOsgViewer()->setSceneData(loadedModel);
-
         //        widget.getOsgViewer()->realize();
 
         return 0;
